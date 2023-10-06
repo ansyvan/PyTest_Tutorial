b0dd1f52ee861e7758bd3fb0d0a46dfc912aa535
Andriana Syvanych <asyvanych@gmail.com>
2023-10-06 12:46:59 +0300

    Corrected code and simplified tests

--- tests/api/test_fixtures.py
+++ tests/api/test_fixtures.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 class User:
 
     def __init__(self):
@@ -12,18 +15,18 @@ class User:
         self.name = ''
         self.second_name = ''
 
+
+@pytest.fixture
+def user():
+    user = User()
+    user.create()
+
+    yield user
+
+    user.remove()
     
-    def test_change_name():
-        user = User()
-        user.create()
+def test_change_name(user):
+    assert user.name == 'Sergii'
 
-        assert user.name == 'Sergii'
-        user.remove()
-
-
-    def test_change_second_name():
-        user = User()
-        user.create()
-
-        assert user.second_name == 'Butenko'
-        user.remove()
+def test_change_second_name(user):
+    assert user.second_name == 'Butenko'

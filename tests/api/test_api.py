9cb404d89dafbca4bd9f9d422acf4a14964dce97
Andriana Syvanych <asyvanych@gmail.com>
2023-10-06 11:58:18 +0300

    Added tests to test_api.py

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,5 +1,21 @@
-def test_check_math():
-    assert 7 * 7 == 49
+import pytest
 
-def test_check_78():
-    assert 7 * 8 == 56
+class User:
+
+    def __init__(self):
+        self.name = 'Sergii'
+        self.second_name = 'Butenko'
+
+@pytest.fixture
+def user():
+    yield User()
+
+def test_remove_name(user):
+    user.name = ''
+    assert user.name == ''
+
+def test_name(user):
+    assert user.name == 'Sergii'
+
+def test_second_name(user):
+    assert user.second_name == 'Butenko'
